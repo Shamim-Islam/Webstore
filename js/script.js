32df29858fd3e43c0cd9717b97e37a3c0bc692fc
Shamim Islam <64851056+Shamim-Islam@users.noreply.github.com>
2020-10-05 16:25:47 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,50 @@
+
+/*------------ Show and hide sticky navbar -------------*/
+
+$(document).ready(function () {
+   
+    // show/hide nav on page load
+    showHideNave();
+    
+    $(window).scroll(function () {
+       
+        // show/hide nav on window's scroll
+        showHideNave();
+        
+    });
+    
+    function showHideNave() {
+        
+        if ($(window).scrollTop () > 150 ) {
+            
+            // show sticky navbar
+            $('nav').addClass('sticky-navbar');
+            
+        } else {
+            
+            // hide sticky navbar
+            $('nav').removeClass('sticky-navbar');
+            
+        }
+        
+    }
+    
+});
+
+/*------------ Mobile menu (Toggler btn) -------------*/
+
+$(document).ready(function () {
+    
+    $('.navbar-toggler').click(function () {
+       
+        $('.navbar-toggler').toggleClass('change');
+        
+    });
+    
+})
+
+/*------------ login form -------------*/
+
 const inputs = document.querySelectorAll('.input');
 
 function focusFunc(){
@@ -16,3 +63,31 @@ inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);    
     input.addEventListener('blur', blurFunc);    
 });
+
+/*------------ smooth scrolling -------------*/
+
+$(document).ready(function () {
+   
+    $('a.smooth-scroll').click(function (event) {
+        
+        event.preventDefault();
+        
+        // get section id
+        var section = $(this).attr('href');
+        
+        $('html, body').animate ({
+           
+            scrollTop: $(section).offset().top - 64
+            
+        }, 1250, 'easeInOutExpo');
+        
+    });
+    
+});
+
+
+
+
+
+
+
